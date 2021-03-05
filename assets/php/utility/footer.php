63483debf6af6e8c6402b484943c86a8af35076f
ncl-a <64961208+ncl-a@users.noreply.github.com>
2021-03-05 21:01:18 +0100

    Cambio nomi footer

--- assets/php/utility/footer.php
+++ assets/php/utility/footer.php
@@ -8,6 +8,6 @@
 						<li><a href="#" class="icon brands fa-google-plus"><span class="label">Google+</span></a></li> -->
 					</ul>
 					<ul class="copyright">
-						<li>&copy; School ticket. Progetto "Idee in Azione", a.s. 2020-2021.</li><li>Design: <a href="#"> Statuti, Giovannetti, Ricciardi, Meglioli, Berselli, Govi, De Almeida, Prandi</a></li>
+						<li>&copy; School ticket. Progetto "Idee in Azione", a.s. 2020-2021.</li><li>Design: <a href="#"> Ricciardi, Giovannetti, Statuti, Meglioli, Berselli, Govi, De Almeida, Prandi</a></li>
 					</ul>
 				</footer>
