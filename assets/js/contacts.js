f57b7924faa763d7ae30a014880186aac16a977a
Meglio2002 <79357701+Meglio2002@users.noreply.github.com>
2021-03-06 12:41:06 +0100

    Add files via upload

---
+++ assets/js/contacts.js
@@ -0,0 +1,93 @@
+document.getElementById("messaggio").addEventListener("click", () => 
+{
+	var nome = document.getElementById("nome").value; // Nome
+	var email = document.getElementById("email").value; // Email
+	var soggetto = document.getElementById("soggetto").value; // Soggetto
+	var messaggio = document.getElementById("messaggio").value; // Messaggio
+	
+});						
+	
+
+/*
+jQuery.ajax(
+{
+	url: 'link',
+	method: 'POST',
+	data: {"nome": nome, "email": email, "soggetto": soggetto, "messaggio": messaggio}
+}).done(function(d)	{
+						jQuery('.target').html(k);
+					});
+
+
+var correct_data = "#67f257";
+var error_data = "#ff5757";
+*/
+
+
+	email.addEventListener("input", () =>
+{
+
+    console.log("email: " + email.value);
+	
+    // se l'email è valida imposto i diversi colori
+    if(validateEmail(email.value))
+	{
+        console.log("email valida");
+        // messaggio utente:
+        document.getElementById("label_email").innerHTML = "Email valida";
+        document.getElementById("label_email").style.color = correct_data;
+
+        email.style.borderColor = correct_data;
+        email.style.color = correct_data;
+        email.style.boxShadow = "0 0 0 2px" + correct_data;
+
+        email_validate = true;
+    }
+	else
+	{
+        console.log("Email NON valida");
+        // messaggio utente:
+        document.getElementById("label_email").innerHTML = "Email non valida";
+        document.getElementById("label_email").style.color = error_data;
+
+        email.style.borderColor = error_data;
+        email.style.color = error_data;
+        email.style.boxShadow = "0 0 0 2px" + error_data;
+
+        email_validate = false;
+    }
+
+    console.log("--------------");
+
+});
+
+// tolto il focus imposto il colore
+email.addEventListener("blur", () =>
+{
+
+    email.style.borderColor = "";
+    email.style.boxShadow = "";
+    email.style.color = "";
+
+    if(email_validate) {
+        email.style.borderColor = correct_data;
+    } else {
+        email.style.borderColor = error_data;
+    }
+
+});
+
+
+function validateEmail (emailAdress)
+{
+  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  
+  if (emailAdress.match(regexEmail))
+  {
+    return true; 
+  }
+  else
+  {
+    return false; 
+  }
+}
