204f14da6b63a4374f4368bf0dc28aa7f56dd7eb
allestatuti02 <allestatuti02@gmail.com>
2021-03-09 10:40:15 +0100

    Update auth.php

--- assets/php/authentication/auth.php
+++ assets/php/authentication/auth.php
@@ -1,25 +1,63 @@
 <?php
-	try
-	{
-		
-		$dsn = "mysql:host=localhost;dbname=magazzino";
-		$username = "root";
-		$password = "";
-		$PDOconn = new PDO($dsn, $username, $password);
-		$PDOconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}catch(PDOExpetion $e){
-		echo $e->getMessage();
-	}
-	
-	//$st = $PDOconn->QUERY("INSERT INTO User(nome, cognome) VALUES (...)");
-	/*$st = $PDOconn->QUERY("SELECT * FROM User");
-		while($record = $st->fetch())
-		{
-			echo "Nome: " .$record['nome']. " Cognome: " .$record['cognome']. <"br">;
+
+	Class Auth{
+		private $PDOconn;
+
+
+	//COSTRUTTORE indica il tipo di variabile nelle()
+		public function __construct(string $host, string $dbName, string $username, string $pass){
+			//PER CONNETTERSI AL DATABASE:
+				try
+				{
+
+					$dsn = "mysql:host=" . $host . ";dbname=" . $dbName;
+					//$username = "root";
+					//$password = "";
+					$this->PDOconn = new PDO($dsn, $username, $pass);
+					$this->PDOconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				}catch(PDOExpetion $e){
+					echo $e->getMessage();
+				}
+
+
 		}
-	*/
-	//rowcount = numero righe
-	//fetch = riga successiva
-	//fetchAll = ritorna righe come arrey
-	
+
+		public function registration($nome, $cognome){
+				$q = "INSERT INTO user (Nome, Cognome) VALUES (:nomePl, :cognomePl)";
+				$st = $this->PDOconn->prepare($q);
+				$st->execute(['nomePl' => $nome, 'cognomePl' => $cognome]);
+		}
+
+		public function login($email, $pssw){
+
+		}
+
+
+	}
+
+	$oggetto = new Auth("localhost","prova","root","");
+
+	$nome = 'Mario';
+	$cognome = 'Rossi';
+	$oggetto->registration($nome, $cognome);
+
+
+	//INSERIRE VALORI IN MANIERA FACILE:
+		//$st = $PDOconn->QUERY("INSERT INTO User(nome, cognome) VALUES (...)");
+		/*$st = $PDOconn->QUERY("SELECT * FROM User");
+	//-----------------------------------------------------------------------------
+
+			while($record = $st->fetch())
+			{
+				echo "Nome: " .$record['nome']. " Cognome: " .$record['cognome']. <"br">;
+			}
+		*/
+		//rowcount = numero righe
+		//fetch = riga successiva
+		//fetchAll = ritorna righe come arrey
+
+
+
+
+
 ?>
