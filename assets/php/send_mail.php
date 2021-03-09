57ca61e76bcd8345679b78bdf5a5bcb88c0600bd
luigi prandi <luigiprandigalaxy02@gmail.com>
2021-03-09 09:40:56 +0100

    ho aggiunto i true e false

--- assets/php/send_mail.php
+++ assets/php/send_mail.php
@@ -1,30 +1,57 @@
 <?php
-	//function send_mail(){
+	function send_mail(){
 		
-		$email 		= "s.luigi.prandi@gobettire.istruzioneer.it"; //$_POST['email']
+		$email 		= $_POST['email'];
 		$emailTO 	= "xssxprova0000@gmail.com"; 
-		$subject	= "Ciao sono la prova (Subject)"; //$_POST['subject']
-		$msg 		= "Email di prova del file send_mail.php"; //$_POST['msg']
+		$subject	= $_POST['subject']
+		$msg 		= $_POST['message'];
+		$name		= $_POST['name'];
 		
+		$headers = "From: " . strip_tags($email) . "\r\n";
+		//$headers .= "Reply-To: ". strip_tags($email) . "\r\n";
+		$headers .= "MIME-Version: 1.0\r\n";
+		$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+		
+		/*$msg = '<html><body>';
+		$msg .= '<h1 align="center">Titolo mail da PHONEIX!</h1>';
+		$msg .= '<table width="20%" border="1" align="center" bgcolor="#FAEBD7">
+					<tr><td> cella 1 </td><td> cella 2 </td></tr>
+					<tr><td> cella a </td><td> cella b </td></tr>
+				</table>';
+		$msg .= '</body></html>';*/
 		
 		
 		
 		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			echo 'False';
+			echo '{"result":false, "description":"email errata"}';
+			return false;
+		}
+				
+		if (!filter_var($name, FILTER_SANITIZE_STRING)) {
+			echo '{"result":false, "description":"nome errato"}';
 			return false;
 		}
 		
+		if (!filter_var($subject, FILTER_SANITIZE_STRING)) {
+			echo '{"result":false, "description":"soggetto errato"}';
+			return false;
+		}
+		
+		if (!filter_var($msg, FILTER_SANITIZE_STRING)) {
+			echo '{"result":false, "description":"messaggio errato"}';
+			return false;
+		}
 		
 		//se email corretto echo True altrimenti echo false
-		if(mail($emailTO, $subject, $msg))
-			ECHO 'True';
+		if(mail($emailTO, $subject, $msg, $headers))
+			ECHO '{"result":true, "description":"email inviata correttamente"}';
 		else
-			ECHO 'False';
+			ECHO '{"result":false, "description":"email non inviata correttamente"}';;
 
 		
-		var_dump($_POST);
+		//var_dump($_POST);
 		
-	//}
+	}
 
 
 
