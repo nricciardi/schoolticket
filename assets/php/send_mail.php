7c03bdfc1368f463e4afd9fcaf9ceedbc67b6b2c
luigi prandi <luigiprandigalaxy02@gmail.com>
2021-03-09 08:30:55 +0100

    ho aggiunto il file per inviare le mail

---
+++ assets/php/send_mail.php
@@ -0,0 +1,28 @@
+<?php
+	//function send_mail(){
+		
+		$email 		= "s.luigi.prandi@gobettire.istruzioneer.it"; //$_POST['email']
+		$emailTO 	= "xssxprova0000@gmail.com"; 
+		$subject	= "Ciao sono la prova (Subject)"; //$_POST['subject']
+		$msg 		= "Email di prova del file send_mail.php"; //$_POST['msg']
+		
+		
+		
+		
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			echo 'False';
+			return false;
+		}
+		
+		
+		//se email corretto echo True altrimenti echo false
+		if(mail($emailTO, $subject, $msg))
+			ECHO 'True';
+		else
+			ECHO 'False';
+
+	//}
+
+
+
+?>
