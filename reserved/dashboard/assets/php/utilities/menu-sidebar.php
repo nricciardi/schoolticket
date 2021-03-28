c117e563d684167f91201d59bfa5709dc689b750
ncl-a <64961208+ncl-a@users.noreply.github.com>
2021-03-28 15:55:41 +0200

    sistami i percorsi per le foto di logo dashboard

--- reserved/dashboard/assets/php/utilities/menu-sidebar.php
+++ reserved/dashboard/assets/php/utilities/menu-sidebar.php
@@ -2,13 +2,13 @@
 <aside class="menu-sidebar2">
             <div class="logo">
                 <a href="#">
-                    <img src="images/icon/logo-white.png" alt="Cool Admin" />
+                    <img src="assets/images/icon/logo-white.png" alt="Cool Admin" />
                 </a>
             </div>
             <div class="menu-sidebar2__content js-scrollbar1">
                 <div class="account2">
                     <div class="image img-cir img-120">
-                        <img src="images/icon/avatar-big-01.jpg" alt="John Doe" />
+                        <img src="assets/images/icon/avatar-big-01.jpg" alt="John Doe" />
                     </div>
                     <h4 class="name">john doe</h4>
                     <a href="#">Sign out</a>
