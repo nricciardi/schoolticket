2b2d27ef9465ab1207022e06b692f2d83e58180b
Gustavolevii <80149834+Gustavolevii@users.noreply.github.com>
2021-03-29 20:48:53 +0200

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,39 +1,96 @@
-function GetAction(data)
-{
-  // istanza dell'oggetto XMLHttpRequest
-  if (window.XMLHttpRequest)
-  {
-    req = new XMLHttpRequest();
-    req.onreadystatechange = ProcessRequest;
-    req.open('POST', url, true);
-    req.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-    req.send(data);
-  }
-  // controllo sulla versione ActiveX
-  else if (window.ActiveXObject)
-  {
-    req = new ActiveXObject('Microsoft.XMLHTTP')
-    if (req)
-    {
-      req.onreadystatechange = ProcessRequest;
-      req.open('POST', url, true);
-      req.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-      req.send(data);
+const HOSTNAME = window.location.protocol + "//" + window.location.hostname + "/" + window.location.pathname.split("/")[1];
+$(document).ready(function() {
+      $("input[type=button]").click(function () {
+      var email = $('input[id=email]').val(); // Utente inserisce email
+      var password = $('input[id=password]').val(); // Utente inserisce password
+	  var data = {"submit": "login", "email": email, "password": password};
+	  console.log(data);
+	  console.log(HOSTNAME + '/assets/php/authentication/auth.php');
+                            $.ajax({
+                            type: "POST",
+                            url: HOSTNAME + '/assets/php/authentication/auth.php',
+                            data : data,
+                            dataType: "text",
+                            success: function (data) {
+                            var success = data['success'];
+                            if(success == false){
+        var error = data['message'];
+                            alert(error); // Nel caso non scriva niente :
+
+
+                                }
+
+                                if(success == true) {
+   $('#mask , .login-popup').fadeOut(300 , function() {
+   $('#mask').remove();  
+                                });// end fadeOut function()
+    setTimeout("location.href = 'home.php';",1000);                                 
+                                                }
+                                                    }
+
+                        });//end ajax             
+                 });//end click function
+         });//end ready function
+		 
+// VARIABILI:
+// - Dato errato
+var error_data = "#ff5757";
+// - Dato corretto
+var correct_data = "#67f257";
+// - Dato incerto
+var warning_data = "#f2d857";
+
+// - Email
+var email = document.getElementById("email");
+var email_validate = false;
+
+if(validateEmail(email.value)) {
+
+        console.log("email valida");
+        // messaggio utente:
+        document.getElementById("label_email").innerHTML = "Email valida, confermala";
+        document.getElementById("label_email").style.color = correct_data;
+
+        email.style.borderColor = correct_data;
+        email.style.color = correct_data;
+        email.style.boxShadow = "0 0 0 2px" + correct_data;
+		email_validate = true;
+	
+    } 
+	else 
+	{
+        console.log("email NON valida");
+        // messaggio utente:
+        document.getElementById("label_email").innerHTML = "Email non valida";
+        document.getElementById("label_email").style.color = error_data;
+
+        email.style.borderColor = error_data;
+        email.style.color = error_data;
+        email.style.boxShadow = "0 0 0 2px" + error_data;
+		email_validate = false;
     }
-  }
+	
+    console.log("--------------");
+});
+
+if(email_validate)
+{
+	document.getElementById("button").removeAttribute("disabled");
+	console.clear();
+} 
+else 
+{
+	document.getElementById("button").setAttribute("disabled", "disabled");
 }
 
-function ProcessRequest()
+function validateEmail (email)
 {
-  // stato della ricerca inviata
-  if (req.readyState == 4)
+  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (email.match(regexEmail)) {
+    return true; 
+  } 
+  else 
   {
-    // stato della ricerca accolta
-    if (req.status == 200)
-    {
-      eval(what);
-    }else{
-      alert('Problema nella gestione dei dati: ' + req.responseText);
-    }
+    return false; 
   }
 }
