e4a6e2c0022301ac0bbfb0b061469f63ba8fac58
De Almeida Silva Gustavo Levi <dealmeidasilvagu@gobetti.lan>
2021-05-22 12:05:30 +0200

    Modifiche

--- reserved/dashboard/assets/php/utilities/management-account.php
+++ reserved/dashboard/assets/php/utilities/management-account.php
@@ -1,219 +1,62 @@
-<div class="row">
-    <div class="col-md-12">
-        <!-- DATA TABLE -->
-        <h3 class="title-5 m-b-35">Account</h3>
-        <div class="table-data__tool">
-            <div class="table-data__tool-left">
-                <div class="rs-select2--light rs-select2--md">
-                    <select class="js-select2" name="property">
-                        <option selected="selected">All Properties</option>
-                        <option value="">Option 1</option>
-                        <option value="">Option 2</option>
-                    </select>
-                    <div class="dropDownSelect2"></div>
-                </div>
-                <div class="rs-select2--light rs-select2--sm">
-                    <select class="js-select2" name="time">
-                        <option selected="selected">Today</option>
-                        <option value="">3 Days</option>
-                        <option value="">1 Week</option>
-                    </select>
-                    <div class="dropDownSelect2"></div>
-                </div>
-                <button class="au-btn-filter">
-                    <i class="zmdi zmdi-filter-list"></i>filters</button>
-                <button type="button" class="btn btn-link" style="color: #6C757D" id="btn_refresh_management_account">
-                    <i class="fa fa-refresh"></i>&nbsp; Ricarica</button>
-            </div>
-            <div class="table-data__tool-right">
-                <button class="au-btn au-btn-icon au-btn--green au-btn--small" id="formAddUser">
-                    <i class="zmdi zmdi-plus"></i>Nuovo utente</button>
-                <!--<div class="rs-select2--dark rs-select2--sm rs-select2--dark2">
-                    <select class="js-select2" name="type">
-                        <option selected="selected">Export</option>
-                        <option value="">Option 1</option>
-                        <option value="">Option 2</option>
-                    </select>
-                    <div class="dropDownSelect2"></div>
-                </div>-->
-            </div>
-        </div>
-        <span id="feedback_table_management_user">N / D</span>
-        <div class="table-responsive table-responsive-data2">
-            <table class="table table-data2">
-                <thead>
-                    <tr>
-                        <th>
-                            <label class="au-checkbox">
-                                <input type="checkbox" id="general_checkbox_account">
-                                <span class="au-checkmark"></span>
-                            </label>
-                        </th>
-                        <!-- Predisposizione IdUtente: <th>Id utente</th> --> 
-                        <th>Cognome</th>
-                        <th>Nome</th>
-                        <th>Email</th>
-                        <!--<th>Password</th>-->
-                        <th>Categoria</th>
-                        <th>Permessi</th>
-                        <th>Azioni</th>
-                    </tr>
-                </thead>
-                <tbody id="body_table_account">
-                    <!--<tr class="tr-shadow">
-                        <td>
-                            <label class="au-checkbox">
-                                <input type="checkbox">
-                                <span class="au-checkmark"></span>
-                            </label>
-                        </td>
-                        <td>Lori Lynch</td>
-                        <td>
-                            <span class="block-email">lori@example.com</span>
-                        </td>
-                        <td class="desc">Samsung S8 Black</td>
-                        <td>2018-09-27 02:12</td>
-                        <td>
-                            <span class="status--process">Processed</span>
-                        </td>
-                        <td>$679.00</td>
-                        <td>
-                            <div class="table-data-feature">
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Send">
-                                    <i class="zmdi zmdi-mail-send"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Edit">
-                                    <i class="zmdi zmdi-edit"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Delete">
-                                    <i class="zmdi zmdi-delete"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="More">
-                                    <i class="zmdi zmdi-more"></i>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr class="spacer"></tr>
-                    <tr class="tr-shadow">
-                        <td>
-                            <label class="au-checkbox">
-                                <input type="checkbox">
-                                <span class="au-checkmark"></span>
-                            </label>
-                        </td>
-                        <td>Lori Lynch</td>
-                        <td>
-                            <span class="block-email">john@example.com</span>
-                        </td>
-                        <td class="desc">iPhone X 64Gb Grey</td>
-                        <td>2018-09-29 05:57</td>
-                        <td>
-                            <span class="status--process">Processed</span>
-                        </td>
-                        <td>$999.00</td>
-                        <td>
-                            <div class="table-data-feature">
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Send">
-                                    <i class="zmdi zmdi-mail-send"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Edit">
-                                    <i class="zmdi zmdi-edit"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Delete">
-                                    <i class="zmdi zmdi-delete"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="More">
-                                    <i class="zmdi zmdi-more"></i>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr class="spacer"></tr>
-                    <tr class="tr-shadow">
-                        <td>
-                            <label class="au-checkbox">
-                                <input type="checkbox">
-                                <span class="au-checkmark"></span>
-                            </label>
-                        </td>
-                        <td>Lori Lynch</td>
-                        <td>
-                            <span class="block-email">lyn@example.com</span>
-                        </td>
-                        <td class="desc">iPhone X 256Gb Black</td>
-                        <td>2018-09-25 19:03</td>
-                        <td>
-                            <span class="status--denied">Denied</span>
-                        </td>
-                        <td>$1199.00</td>
-                        <td>
-                            <div class="table-data-feature">
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Send">
-                                    <i class="zmdi zmdi-mail-send"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Edit">
-                                    <i class="zmdi zmdi-edit"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Delete">
-                                    <i class="zmdi zmdi-delete"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="More">
-                                    <i class="zmdi zmdi-more"></i>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr class="spacer"></tr>
-                    <tr class="tr-shadow">
-                        <td>
-                            <label class="au-checkbox">
-                                <input type="checkbox">
-                                <span class="au-checkmark"></span>
-                            </label>
-                        </td>
-                        <td>Lori Lynch</td>
-                        <td>
-                            <span class="block-email">doe@example.com</span>
-                        </td>
-                        <td class="desc">Camera C430W 4k</td>
-                        <td>2018-09-24 19:10</td>
-                        <td>
-                            <span class="status--process">Processed</span>
-                        </td>
-                        <td>$699.00</td>
-                        <td>
-                            <div class="table-data-feature">
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Send">
-                                    <i class="zmdi zmdi-mail-send"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Edit">
-                                    <i class="zmdi zmdi-edit"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Delete">
-                                    <i class="zmdi zmdi-delete"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="More">
-                                    <i class="zmdi zmdi-more"></i>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>-->
-                </tbody>
-                <tfoot id="foot_table_account">
-                    <tr class="spacer"></tr>
-                    <tr class="tr-shadow">
-                        <td>
-                            <button type="button" class="btn btn-danger" id="btn_delete_checked_user" disabled>
-                                <i class="fas fa-trash-alt"></i><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">&nbsp; Cancella 0 utenti selezionati</font></font>
-                            </button>
-                        </td>
-                    </tr>
-                </tfoot>
-            </table>
-        </div>
-        <!-- END DATA TABLE -->
-    </div>
-</div>
 
+							<div class="col-lg-12">
+								<div class="card">
+                                    <div class="card-header">Visualizzazione Account</div><br>
+                                    <div class="div_form_show_account">
+                                        <form action="" method="post" class="">
+											<div class="form-group">
 
+                                                <div class="row">
+                                                    <div class="col-9">
+                                                        <div class="input-group">
+                                                            <div class="input-group-addon"> Nome </div>
+                                                            <input type="text" id="codice" name="codice" class="form-control">
+                                                            <div class="input-group-addon">
+                                                                <i class="fas fa-key"></i>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <!--<br>-->
+                                                    <div class="col-3">
+                                                        <div class="card-footer" style="border-top: none; padding: 0;">
+                                                            <button type="button" class="btn btn-info" id="inviacodice" style="float: right;">
+                                                                Invia codice
+                                                            </button>
+                                                        </div>
+                                                    </div>
+													
+                                                </div>
+											</div><!--<br>--><span id="span2"> </span>
+											
+                                            <div class="form-group" style="border-top: 1px solid rgba(0,0,0,.125); padding-top: 0.75rem">
+                                                <div class="input-group">
+                                                    <div class="input-group-addon"> Nuova Password </div>
+                                                    <input type="password" id="nuovaPassword" name="password" class="form-control">
+                                                    <div class="input-group-addon">
+                                                        <i class="fa fa-asterisk"></i>
+                                                    </div>
+                                                </div><br><span id="span3"> </span>
+												<div class="input-group">
+                                                    <div class="input-group-addon"> Conferma Password </div>
+                                                    <input type="password" id="confermaPassword" name="confermaPassword" class="form-control">
+                                                    <div class="input-group-addon">
+                                                        <i class="fa fa-asterisk"></i>
+                                                    </div>
+                                                </div><br><span id="span4"> </span>
+                                            </div>
+											
+											<div class="card-footer">
+												<button type="button" class="btn btn-success btn-sm " id="bottone" disabled>
+													<i class="fas fa-check"></i> Conferma
+												</button>
+												<button type="reset" class="btn btn-danger btn-sm">
+													<i class="fa fa-ban"></i> Reset
+												</button>
+												<span id="span"> In attesa dell'inserimento </span>
+											</div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+							
+							<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
