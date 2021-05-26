a6d92bd42a5ff9299c4f4c84bedd77fcc11a80a7
Meglio2002 <gabrielemeglioli02@gmail.com>
2021-05-26 11:49:57 +0200

    merge

--- reserved/dashboard/assets/php/utilities/management-macroarea.php
+++ reserved/dashboard/assets/php/utilities/management-macroarea.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-md-12">
         <!-- DATA TABLE -->
-        <h3 class="title-5 m-b-35">Utenti</h3>
+        <h3 class="title-5 m-b-35">Macroaree</h3>
         <div class="table-data__tool">
             <div class="table-data__tool-left">
                 <div class="rs-select2--light rs-select2--md">
@@ -22,12 +22,12 @@
                 </div>
                 <button class="au-btn-filter">
                     <i class="zmdi zmdi-filter-list"></i>filters</button>
-                <button type="button" class="btn btn-link" style="color: #6C757D" id="btn_refresh_management_user">
+                <button type="button" class="btn btn-link" style="color: #6C757D" id="btn_refresh_management_macroarea">
                     <i class="fa fa-refresh"></i>&nbsp; Ricarica</button>
             </div>
             <div class="table-data__tool-right">
-                <button class="au-btn au-btn-icon au-btn--green au-btn--small" id="formAddUser">
-                    <i class="zmdi zmdi-plus"></i>Nuovo utente</button>
+                <button class="au-btn au-btn-icon au-btn--green au-btn--small" id="formAddMacroarea">
+                    <i class="zmdi zmdi-plus"></i>Nuova macroarea</button>
                 <!--<div class="rs-select2--dark rs-select2--sm rs-select2--dark2">
                     <select class="js-select2" name="type">
                         <option selected="selected">Export</option>
@@ -48,16 +48,12 @@
                                 <span class="au-checkmark"></span>
                             </label>
                         </th>
-                        <!-- Predisposizione IdUtente: <th>Id utente</th> --> 
-                        <th>Cognome</th>
+                        <!-- Predisposizione IdMacroarea: <th>Id Macroarea</th> --> 
                         <th>Nome</th>
-                        <th>Email</th>
-                        <th>Categoria</th>
-                        <th>Permessi</th>
-                        <th>Azioni</th>
+                        <th>Descrizione</th>
                     </tr>
                 </thead>
-                <tbody id="body_table_users">
+                <tbody id="body_table_macroarea">
                     <!--<tr class="tr-shadow">
                         <td>
                             <label class="au-checkbox">
@@ -198,7 +194,7 @@
                         </td>
                     </tr>-->
                 </tbody>
-                <tfoot id="foot_table_users">
+                <tfoot id="foot_table_macroarea">
                 
                 </tfoot>
             </table>
