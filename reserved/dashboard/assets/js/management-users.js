e3ebfa56fe295f292a3b6e4b84de94e55e4bb001
Meglio2002 <gabrielemeglioli02@gmail.com>
2021-05-28 11:52:59 +0200

    merge

--- reserved/dashboard/assets/js/management-users.js
+++ reserved/dashboard/assets/js/management-users.js
@@ -213,7 +213,7 @@ function createTableUser() {
 }
 
 // in base all'id passato cerco di creare un nuovo utente
-function addUser() {
+async function addUser() {
     console.log("Aggiungo un utente");
 
     // controllo che tutti i controlli siano andati a buon fine
@@ -243,7 +243,7 @@ function addUser() {
             } else {
 
                 // in caso positivo creo la tabella per gli utenti
-                createTableUser();
+                await createTableUser();
 
                 // in caso di successo stampo un messaggio nel box al posto della tabella
                 feedback_table_management_user.innerText = res.description;
@@ -266,7 +266,7 @@ function addUser() {
 }
 
 // in base all'id passato elimino l'utente
-function editUser(ID) {   // può anche essere passato un array
+async function editUser(ID) {   // può anche essere passato un array
     
     console.log("Modifico: " + ID);
 
@@ -293,7 +293,7 @@ function editUser(ID) {   // può anche essere passato un array
             } else {
 
                 // in caso positivo creo la tabella per gli utenti
-                createTableUser();
+                await createTableUser();
 
             }
 
@@ -312,7 +312,7 @@ function editUser(ID) {   // può anche essere passato un array
 }
 
 // in base all'id passato elimino l'utente
-function deleteUser(ID) {   // può anche essere passato un array
+async function deleteUser(ID) {   // può anche essere passato un array
     
     console.log("Elimino: " + ID);
 
@@ -339,7 +339,7 @@ function deleteUser(ID) {   // può anche essere passato un array
             } else {
 
                 // in caso positivo creo la tabella per gli utenti
-                createTableUser();
+                await createTableUser();
 
             }
 
@@ -684,12 +684,12 @@ general_checkbox_users.addEventListener("change", () => {
 });
 
 // aggiungo il form per l'aggiunta di un nuovo utente
-form_add_user.addEventListener("click", () => {
+form_add_user.addEventListener("click", async () => {
 
     console.log("Aggiunto il form");
 
     // aggiungo il form all'inzio del codice già esistente
-    createTableUser();
+    await createTableUser();
     let actual_body = body_table_users.innerHTML
     body_table_users.innerHTML = createFormNewUser() + actual_body; 
 
@@ -699,14 +699,14 @@ form_add_user.addEventListener("click", () => {
 });
 
 // ricarico la tabella riaggiungendola al click del bottone di refresh
-btn_refresh_management_user.addEventListener("click", () => {
+btn_refresh_management_user.addEventListener("click", async () => {
 
     
 
     // disabilito il bottone per 3 secondi
     
     // creo la tabella
-    createTableUser();
+    await createTableUser();
 
     // disabilito il bottone
     btn_refresh_management_user.disabled = true;
