3a16564f5b1e7938fb7a7aaca09eee6e6fe945ef
Gustavolevii <80149834+Gustavolevii@users.noreply.github.com>
2021-06-01 12:29:27 +0200

    management account

--- reserved/dashboard/assets/php/utilities/management-account.php
+++ reserved/dashboard/assets/php/utilities/management-account.php
@@ -3,184 +3,54 @@
         <!-- DATA TABLE -->
         <h3 class="title-5 m-b-35">Account</h3>
         <div class="table-data__tool">
-            <div class="table-data__tool-left">
-                <button type="button" class="btn btn-link" style="color: #6C757D" id="btn_refresh_management_account">
-                    <i class="fa fa-refresh"></i>&nbsp; Ricarica</button>
-            </div>
+			<span id="feedback_table_management_account">N / D</span>
             <div class="table-data__tool-right">
-                <!--<div class="rs-select2--dark rs-select2--sm rs-select2--dark2">
-                    <select class="js-select2" name="type">
-                        <option selected="selected">Export</option>
-                        <option value="">Option 1</option>
-                        <option value="">Option 2</option>
-                    </select>
-                    <div class="dropDownSelect2"></div>
-                </div>-->
             </div>
         </div>
         <div class="table-responsive table-responsive-data2">
             <table class="table table-data2">
                 <thead>
                     <tr>
-                        <th>
-                            <label class="au-checkbox">
-                                <input type="checkbox" id="general_checkbox_account">
-                                <span class="au-checkmark"></span>
-                            </label>
-                        </th>
                         <!-- Predisposizione IdUtente: <th>Id utente</th> --> 
                         <th>Cognome</th>
-                        <th>Nome</th>
-                        <th>Email</th>
+						<td><span id = "Cgnm">N / D</span></td>
+                        <td></td>
+                    </tr>
+					<tr>
+						<th> Nome </th>
+						<td><span id = "Nm">N / D</span></td>
+						<td></td>
+					<tr>
+						<th>Email</th>
+						<td><span id="Eml">N / D</span></td>
+						<td></td>
+					</tr>
+                    <tr>
                         <th>Password</th>
-						<th>Categoria</th>
-                    </tr>
+						<td>**********</td>
+						<td></td>
+					</tr>
+					<tr>
+                        <th>Categoria</th>
+						<td><span id="Ctgr">N / D</span></td>
+						<td></td>
+					</tr>
+					<tr>
+                        <th>Permessi</th>
+						<td><span id="Prmss">N / D</span></td>
+						<td></td>
+					</tr>
                 </thead>
-                <tbody id="body_table_account">
-                    <!--<tr class="tr-shadow">
-                        <td>
-                            <label class="au-checkbox">
-                                <input type="checkbox">
-                                <span class="au-checkmark"></span>
-                            </label>
-                        </td>
-                        <td>Lori Lynch</td>
-                        <td>
-                            <span class="block-email">lori@example.com</span>
-                        </td>
-                        <td class="desc">Samsung S8 Black</td>
-                        <td>2018-09-27 02:12</td>
-                        <td>
-                            <span class="status--process">Processed</span>
-                        </td>
-                        <td>$679.00</td>
-                        <td>
-                            <div class="table-data-feature">
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Send">
-                                    <i class="zmdi zmdi-mail-send"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Edit">
-                                    <i class="zmdi zmdi-edit"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Delete">
-                                    <i class="zmdi zmdi-delete"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="More">
-                                    <i class="zmdi zmdi-more"></i>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr class="spacer"></tr>
-                    <tr class="tr-shadow">
-                        <td>
-                            <label class="au-checkbox">
-                                <input type="checkbox">
-                                <span class="au-checkmark"></span>
-                            </label>
-                        </td>
-                        <td>Lori Lynch</td>
-                        <td>
-                            <span class="block-email">john@example.com</span>
-                        </td>
-                        <td class="desc">iPhone X 64Gb Grey</td>
-                        <td>2018-09-29 05:57</td>
-                        <td>
-                            <span class="status--process">Processed</span>
-                        </td>
-                        <td>$999.00</td>
-                        <td>
-                            <div class="table-data-feature">
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Send">
-                                    <i class="zmdi zmdi-mail-send"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Edit">
-                                    <i class="zmdi zmdi-edit"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Delete">
-                                    <i class="zmdi zmdi-delete"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="More">
-                                    <i class="zmdi zmdi-more"></i>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr class="spacer"></tr>
-                    <tr class="tr-shadow">
-                        <td>
-                            <label class="au-checkbox">
-                                <input type="checkbox">
-                                <span class="au-checkmark"></span>
-                            </label>
-                        </td>
-                        <td>Lori Lynch</td>
-                        <td>
-                            <span class="block-email">lyn@example.com</span>
-                        </td>
-                        <td class="desc">iPhone X 256Gb Black</td>
-                        <td>2018-09-25 19:03</td>
-                        <td>
-                            <span class="status--denied">Denied</span>
-                        </td>
-                        <td>$1199.00</td>
-                        <td>
-                            <div class="table-data-feature">
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Send">
-                                    <i class="zmdi zmdi-mail-send"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Edit">
-                                    <i class="zmdi zmdi-edit"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Delete">
-                                    <i class="zmdi zmdi-delete"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="More">
-                                    <i class="zmdi zmdi-more"></i>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr class="spacer"></tr>
-                    <tr class="tr-shadow">
-                        <td>
-                            <label class="au-checkbox">
-                                <input type="checkbox">
-                                <span class="au-checkmark"></span>
-                            </label>
-                        </td>
-                        <td>Lori Lynch</td>
-                        <td>
-                            <span class="block-email">doe@example.com</span>
-                        </td>
-                        <td class="desc">Camera C430W 4k</td>
-                        <td>2018-09-24 19:10</td>
-                        <td>
-                            <span class="status--process">Processed</span>
-                        </td>
-                        <td>$699.00</td>
-                        <td>
-                            <div class="table-data-feature">
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Send">
-                                    <i class="zmdi zmdi-mail-send"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Edit">
-                                    <i class="zmdi zmdi-edit"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="Delete">
-                                    <i class="zmdi zmdi-delete"></i>
-                                </button>
-                                <button class="item" data-toggle="tooltip" data-placement="top" title="More">
-                                    <i class="zmdi zmdi-more"></i>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>-->
+                <tbody id="body_table_accounts">
+                <!-- BODY TABLE -->
                 </tbody>
-                <tfoot id="foot_table_account">
+                <tfoot id="foot_table_accounts">
                     <tr class="spacer"></tr>
                     <tr class="tr-shadow">
                         <td>
+                            <button type="button" class="btn btn-danger" id="btn_delete_checked_account" disabled>
+                                <i class="fas fa-trash-alt"></i><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">&nbsp; Cancella 0 utenti selezionati</font></font>
+                            </button>
                         </td>
                     </tr>
                 </tfoot>
